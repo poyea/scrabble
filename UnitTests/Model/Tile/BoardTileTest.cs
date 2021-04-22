257edfe7fe87af9f1e99c2c09fb9a1fa42e06e1e
Ruifeng Wang <rwang@seattleu.edu>
2021-04-21 20:24:46 -0700

    Add file for BoardTileTest.cs to project

---
+++ UnitTests/Model/Tile/BoardTileTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Scrabble2018;
+using Scrabble2018.Model;
+using System;
+
+namespace UnitTests
+{
+    public class BoardTileTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
