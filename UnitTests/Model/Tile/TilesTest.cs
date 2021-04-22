c404f3fde3fdc0e03db905c605fa53ddd267bffe
Ruifeng Wang <rwang@seattleu.edu>
2021-04-21 20:04:32 -0700

    Add Tiles_TileChar_Set_Should_Return_d to TilesTest.cs

--- UnitTests/Model/Tile/TilesTest.cs
+++ UnitTests/Model/Tile/TilesTest.cs
@@ -24,5 +24,21 @@ namespace UnitTests
             // Assert
             Assert.AreEqual('c', result);
         }
+
+        [Test]
+        public void Tiles_TileChar_Set_Should_Return_d()
+        {
+            // Arrange
+            Tile tile = new Tile('c', 10);
+            tile.TileChar = 'd';
+
+            // Act
+            var result = tile.TileChar;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual('d', result);
+        }
     }
 }
