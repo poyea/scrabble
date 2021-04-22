f340ccfdbb40ea17c16f4fde8c580d99314e8da5
Ruifeng Wang <rwang@seattleu.edu>
2021-04-21 20:03:23 -0700

    Add file TilesTest.cs

---
+++ UnitTests/Model/Tile/TilesTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using Scrabble2018.Model;
+
+namespace UnitTests
+{
+    public class TilesTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }       
+    }
+}
