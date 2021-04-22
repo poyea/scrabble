81daa5a2258879dc32a013e28c7eec796041da03
Ruifeng Wang <rwang@seattleu.edu>
2021-04-21 22:23:23 -0700

    Add BoardTile_WordMultiplier_WordTriple_Should_Return_3 to BoardTileTest.cs

--- UnitTests/Model/Tile/BoardTileTest.cs
+++ UnitTests/Model/Tile/BoardTileTest.cs
@@ -7,9 +7,25 @@ namespace UnitTests
 {
     public class BoardTileTest
     {
+        BoardTiles bt;
         [SetUp]
         public void Setup()
         {
+            bt = new BoardTiles();
+        }
+
+        [Test]
+        public void BoardTile_WordMultiplier_WordTriple_Should_Return_3()
+        {
+            // Arrange
+
+            // Act
+            var result = bt.WordMultiplier(0, 0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(3, result);
         }
     }
 }
