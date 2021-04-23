b1a56d409164243e464498cd2052e47b02074b1e
Miriam Mnyuku <mmnyuku@seattleu.edu>
2021-04-22 17:18:56 -0700

    Set up for testing in ScoreUtilityTest.cs

--- UnitTests/Model/Word/ScoreUtilityTests.cs
+++ UnitTests/Model/Word/ScoreUtilityTests.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using Scrabble2018;
+using Scrabble2018.Model.Word;
+using System.Collections.Generic;
 
 namespace UnitTests
 {
@@ -7,6 +10,29 @@ namespace UnitTests
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public static void ScoreUtilityTests_ScoreCalc_Return_corretScoreSum()
+        {
+            //private List<int> WordMultiply;
+
+            //Arrange
+            ScoreUtility score = new ScoreUtility();
+
+            List<int> WordMultiply = new List<int>();
+            BoardTiles bt = new BoardTiles();
+            int fix, j, jM;
+            string direction;
+            char[,] b;
+
+            //Act
+            var result = score.ScoreCalc(fix, j, jM, direction, b, bt);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(sum, result);
         }
     }
 }
