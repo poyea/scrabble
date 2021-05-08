ffe51535dd4a619640f85ca71e21b14804144254
Ruifeng Wang <rwang@seattleu.edu>
2021-05-07 22:11:44 -0700

    Add MoveRecorder_Constructor_Should_Not_Be_Null to MoveRecorderTest.cs

---
+++ UnitTests/Model/MoveRecorderTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Scrabble2018;
+using Scrabble2018.Model;
+using System;
+
+namespace UnitTests
+{
+    public class MoveRecorderTest
+    {
+        MoveRecorder mr;
+
+        [SetUp]
+        public void Setup()
+        {
+            mr = new MoveRecorder();
+        }
+
+        [Test]
+        public void MoveRecorder_Constructor_Should_Not_Be_Null()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsNotNull(mr);
+            Assert.AreEqual(0, mr.Index.Count);
+            Assert.AreEqual(0, mr.Moves.Count);
+        }
+    }
+}
