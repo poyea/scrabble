3305393ed96c41478c980bf03cefca237485d460
Ruifeng Wang <rwang@seattleu.edu>
2021-05-07 23:01:55 -0700

    Add GameStartDraw_Draw_Should_Pass to GameStartDrawTest.cs

--- UnitTests/Model/Game/GameStartDrawTest.cs
+++ UnitTests/Model/Game/GameStartDrawTest.cs
@@ -1,4 +1,8 @@
 using NUnit.Framework;
+using Scrabble2018;
+using Scrabble2018.Model;
+using Scrabble2018.Model.Game;
+using System;
 
 namespace UnitTests
 {
@@ -7,6 +11,20 @@ namespace UnitTests
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void GameStartDraw_Draw_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            GameStartDraw.Draw();
+
+            // Assert
+            Assert.IsTrue(true);
+
+            // Reset
         }
     }
 }
